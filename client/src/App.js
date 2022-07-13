258abde43952da5149b01b95e7c237e88f61dd6c
boluwatifeBE <boluwatifeemmanuel.be@gmail.com>
2022-07-13 14:22:19 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Profile from './pages/profile/Profile';
+import Register from './pages/register/Register';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
+
+function App() {
+	const { user } = useContext(AuthContext);
+	const clearCacheData = () => {
+		caches.keys().then((names) => {
+			names.forEach((name) => {
+				caches.delete(name);
+			});
+		});
+		alert('Complete Cache Cleared');
+	};
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/'>
+					{user ? <Home /> : <Register />}
+				</Route>
+				<Route path='/login'>{user ? <Redirect to='/' /> : <Login />}</Route>
+				<Route path='/register'>
+					{user ? <Redirect to='/' /> : <Register />}
+				</Route>
+				<Route path='/profile/:username'>
+					<Profile />
+				</Route>
+			</Switch>
+		</Router>
+	);
+}
+
+export default App;
